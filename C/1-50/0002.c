3686588e711d2e2bb287d759b04aa5602354e868
Jacob <jacob.t.wall@gmail.com>
2012-04-21 20:05:00 -0700

    solution to problem 2 in C

---
+++ C/1-50/0002.c
@@ -0,0 +1,26 @@
+/*
+ * Project Euler - Problem 2
+ * 
+ * By considering the terms in the Fibonacci sequence whose values do not
+ * exceed four million, find the sum of the even-valued terms.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int sum = 0;
+    
+    int a = 1, b = 2;
+    
+    while (b < 4000000) {
+        if (b % 2 == 0)
+           sum += b;
+           
+        a = b;
+        b = b + a;
+    }
+    
+    printf("%d", sum);
+    
+    return 0;
+}
